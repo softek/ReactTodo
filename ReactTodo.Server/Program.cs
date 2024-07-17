731291495d1c22ca0c42037b242345986fe41bf2
Eric Lamb <eric.lamb@softekinc.com>
2024-07-17 10:11:40 -0400

    Converts from Swashbuckel to NSwag
    Swashbuckle hasn't been maintained for a while and is being dropped from the AspNetCore templates in .Net 9. Additionally, we want the specification json output from NSwag to feed RTK Query's code generation.
    
    https://github.com/dotnet/aspnetcore/issues/54599

--- ReactTodo.Server/Program.cs
+++ ReactTodo.Server/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
@@ -15,8 +15,8 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
