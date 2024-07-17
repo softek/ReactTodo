9e6b7847ee44aaf3fb3aab16e313ccbc7ec818cf
Eric Lamb <eric.lamb@softekinc.com>
2024-07-17 14:38:40 -0400

    Configures eslint

--- reacttodo.client/.eslintrc.cjs
+++ reacttodo.client/.eslintrc.cjs
@@ -1,14 +1,53 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended', "plugin:react/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules:{
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.stories.*',
+      ],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 }
