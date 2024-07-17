8fe850d13faf041d9425674474006dfcd0144b9f
Eric Lamb <eric.lamb@softekinc.com>
2024-07-17 15:36:28 -0400

    Configures eslint integration with vite
    Shows eslint errors when running application and clears them as they are fixed. This will enforce better

--- reacttodo.client/vite.config.ts
+++ reacttodo.client/vite.config.ts
@@ -6,6 +6,7 @@ import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
+import eslint from 'vite-plugin-eslint';
 
 const baseFolder = env.APPDATA !== undefined && env.APPDATA !== ''
   ? `${env.APPDATA}/ASP.NET/https`
@@ -34,7 +35,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin()],
+  plugins: [plugin(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
