40a53cd011db4c08af0561a167657a8f6c0c2308
Eric Lamb <eric.lamb@softekinc.com>
2024-07-17 10:35:13 -0400

    Adds RTK Query client generation
    Disables hook generation due to current type portability issue.
    
    https://github.com/reduxjs/redux-toolkit/pull/4467

---
+++ reacttodo.client/src/app/api/api-generated.ts
@@ -0,0 +1,22 @@
+import { emptyApi as api } from "./api-base";
+const injectedRtkApi = api.injectEndpoints({
+    endpoints: (build) => ({
+        weatherForecastGet: build.query<
+            WeatherForecastGetApiResponse,
+            WeatherForecastGetApiRequest
+        >({
+            query: () => ({ url: `/WeatherForecast` }),
+        }),
+    }),
+    overrideExisting: false,
+});
+export { injectedRtkApi as api };
+export type WeatherForecastGetApiResponse =
+    /** status 200  */ WeatherForecast[];
+export type WeatherForecastGetApiRequest = void;
+export type WeatherForecast = {
+    date?: string;
+    temperatureC?: number;
+    temperatureF?: number;
+    summary?: string | null;
+};
