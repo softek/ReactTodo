f9e6cfd4c8569f8fa9d008fe2f2cb7a513be3ce1
Eric Lamb <eric.lamb@softekinc.com>
2024-07-19 14:15:53 -0400

    Removes project template weather forecast items
    Removes the weather forecast list that is part of the default dot-net React template.

--- reacttodo.client/src/app/api/api-generated.ts
+++ reacttodo.client/src/app/api/api-generated.ts
@@ -1,18 +1,11 @@
 import { emptyApi as api } from "./api-base";
-export const addTagTypes = ["WeatherForecast", "TodoLists"] as const;
+export const addTagTypes = ["TodoLists"] as const;
 const injectedRtkApi = api
     .enhanceEndpoints({
         addTagTypes,
     })
     .injectEndpoints({
         endpoints: (build) => ({
-            weatherForecastGet: build.query<
-                WeatherForecastGetApiResponse,
-                WeatherForecastGetApiRequest
-            >({
-                query: () => ({ url: `/WeatherForecast` }),
-                providesTags: ["WeatherForecast"],
-            }),
             createTodoListHandle: build.mutation<
                 CreateTodoListHandleApiResponse,
                 CreateTodoListHandleApiRequest
@@ -35,21 +28,12 @@ const injectedRtkApi = api
         overrideExisting: false,
     });
 export { injectedRtkApi as api };
-export type WeatherForecastGetApiResponse =
-    /** status 200  */ WeatherForecast[];
-export type WeatherForecastGetApiRequest = void;
 export type CreateTodoListHandleApiResponse = /** status 200  */ TodoListDto;
 export type CreateTodoListHandleApiRequest = {
     todoListCreateDto: TodoListCreateDto;
 };
 export type ListTodoListsHandleApiResponse = /** status 200  */ TodoListDto[];
 export type ListTodoListsHandleApiRequest = void;
-export type WeatherForecast = {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary?: string | null;
-};
 export type TodoListDto = {
     id: string;
     name: string;
